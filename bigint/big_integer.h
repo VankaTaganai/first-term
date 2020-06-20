4a4570b1b8be86d48600776e9d7273c74059d9e4
VankaTaganai <panovivan01@yandex.ru>
2020-06-20 13:40:39 +0300

    bigint added

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -1,15 +1,16 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
 
+#include "vector.h"
 #include <cstddef>
-#include <gmp.h>
 #include <iosfwd>
+#include <cstdint>
 
-struct big_integer
-{
+struct big_integer {
     big_integer();
     big_integer(big_integer const& other);
     big_integer(int a);
+    big_integer(uint32_t a);
     explicit big_integer(std::string const& str);
     ~big_integer();
 
@@ -48,7 +49,26 @@ struct big_integer
     friend std::string to_string(big_integer const& a);
 
 private:
-    mpz_t mpz;
+    size_t length() const;
+
+    big_integer abs() const;
+
+    void expand(size_t len);
+
+    big_integer binpow2(size_t n);
+
+    uint32_t get_byte(size_t i) const;
+
+    void shrink();
+
+    friend uint32_t trial(__uint128_t a, __uint128_t b, __uint128_t c, __uint128_t d, __uint128_t e);
+    friend bool smaller(big_integer const &r, big_integer const &dq, size_t k, size_t m);
+    friend big_integer quotient(big_integer const& y, uint32_t k);
+    friend void difference(big_integer &r, big_integer const &dq, size_t k, size_t m);
+
+private:
+    bool negative;
+    vector<uint32_t> num;
 };
 
 big_integer operator+(big_integer a, big_integer const& b);
@@ -63,13 +83,6 @@ big_integer operator^(big_integer a, big_integer const& b);
 
 big_integer operator<<(big_integer a, int b);
 big_integer operator>>(big_integer a, int b);
-
-bool operator==(big_integer const& a, big_integer const& b);
-bool operator!=(big_integer const& a, big_integer const& b);
-bool operator<(big_integer const& a, big_integer const& b);
-bool operator>(big_integer const& a, big_integer const& b);
-bool operator<=(big_integer const& a, big_integer const& b);
-bool operator>=(big_integer const& a, big_integer const& b);
 
 std::string to_string(big_integer const& a);
 std::ostream& operator<<(std::ostream& s, big_integer const& a);
